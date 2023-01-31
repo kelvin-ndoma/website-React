fdb650082291e9a12c459775db67d4635786bf19
kevin.mutua@student.moringaschool.com <kevin.mutua@student.moringaschool.com>
2023-01-31 16:09:03 +0300

    added styles

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import Me from '../../assets/image1.png'
+import HeaderSocial from './HeaderSocial'
 
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header_container">
+        <h5>Hello I'm</h5>
+        <h1>Kelvin Ndoma</h1>
+        <h5 className='text-light'>Developer</h5>
+        <CTA/>
+        <HeaderSocial/>
+        <div className='me'>
+          <img src={Me} alt=""/>
+        </div>
+        <a href="#contact" className="scroll_down">Scroll Down</a>
+        </div>
+
+    </header>
   )
 }
 
--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import './navbar.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BiBookHeart} from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri'
+import {BiMessageRounded} from 'react-icons/bi'
 
 function Navbar() {
   return (
-    <div>navbar</div>
+    <nav>
+      <a href="#"><AiOutlineHome/></a>
+      <a href="#about"><AiOutlineUser/></a>
+      <a href="#experience"><BiBookHeart/></a>
+      <a href="#services"><RiServiceLine/></a>
+      <a href="#content"><BiMessageRounded/></a>
+      
+    </nav>
   )
 }
 
